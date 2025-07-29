9575ca11a241b15ec84e8c9f9982cfb05ab121c0
Gisele <giseleptbr@hotmail.com>
2025-07-29 20:11:26 -0300

    Implementa classes de smartphone, Nokia e iPhone com POO

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DesafioPOO.Models;
+
+class Program
+{
+    static void Main()
+    {
+        Smartphone nokia = new Nokia("3499999999", "Nokia G21", "111111111111", 64);
+        Console.WriteLine("=== NOKIA ===");
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("1199999999", "iPhone 13", "222222222222", 128);
+        Console.WriteLine("=== IPHONE ===");
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
